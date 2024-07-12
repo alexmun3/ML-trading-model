bb56766fee4a3606501af6bab797f23ffd334db8
alexmun3 <mundenalex06@gmail.com>
2024-07-12 09:23:59 +0100

    .
    .

---
+++ dataset.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+api_key = '79fWD7OC8t6l1usTOZcvX3mD1H94jNAs8AUnhTjs'
+
+url='https://api.chart-img.com/v2/tradingview/advanced-chart'
+forex_pair = 'EURUSD'
+
+headers = {
+    'x-api-key': api_key,
+    'content-type': 'application/json'
+   }
+
+payload = {
+    "symbol": "OANDA:EURUSD",
+    "interval": "4h",
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+if response.status_code == 200:
+    with open('chart-img-advanced.png', 'wb') as file:
+        file.write(response.content)
+    
+    print('Chart image saved as chart-img.png')
+    
+else:
+    print('Failed to get the chart image:', response.text)
